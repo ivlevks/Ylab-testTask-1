1f68d12a10026aa61b8a9bdce78694d25fd33747
ivlevks <ivlevks@yandex.ru>
2023-03-11 20:50:45 +0300

    check Null, refactor

--- src/main/java/com/ivlevks/hw2/complex_numbers/ComplexNumbersImpl.java
+++ src/main/java/com/ivlevks/hw2/complex_numbers/ComplexNumbersImpl.java
@@ -2,54 +2,74 @@ package com.ivlevks.hw2.complex_numbers;
 
 import java.util.Objects;
 
-public class ComplexNumber {
-    private final double Re;
-    private final double Im;
+public class ComplexNumbersImpl implements ComplexNumbers {
+    private double Re;
+    private double Im;
 
-    public ComplexNumber(double re) {
+    public ComplexNumbersImpl() {
+    }
+
+    public ComplexNumbersImpl(double re) {
         Re = re;
         Im = 0;
     }
 
-    public ComplexNumber(double re, double im) {
+    public ComplexNumbersImpl(double re, double im) {
         Re = re;
         Im = im;
     }
 
-    public static ComplexNumber add(ComplexNumber first, ComplexNumber second) {
+    @Override
+    public ComplexNumbersImpl add(ComplexNumbersImpl first, ComplexNumbersImpl second) {
+
+        checkArgumentsForNull(first, second);
+
         double resultRe = first.Re + second.Re;
         double resultIm = first.Im + second.Im;
 
         checkDoubleOverflow(resultRe, resultIm);
 
-        return new ComplexNumber(resultRe, resultIm);
+        return new ComplexNumbersImpl(resultRe, resultIm);
     }
 
-    public static ComplexNumber subtract(ComplexNumber first, ComplexNumber second) {
+    @Override
+    public ComplexNumbersImpl subtract(ComplexNumbersImpl first, ComplexNumbersImpl second) {
+
+        checkArgumentsForNull(first, second);
+
         double resultRe = first.Re - second.Re;
         double resultIm = first.Im - second.Im;
 
         checkDoubleOverflow(resultRe, resultIm);
 
-        return new ComplexNumber(resultRe, resultIm);
+        return new ComplexNumbersImpl(resultRe, resultIm);
     }
 
-    public static ComplexNumber multiply(ComplexNumber first, ComplexNumber second) {
+    @Override
+    public ComplexNumbersImpl multiply(ComplexNumbersImpl first, ComplexNumbersImpl second) {
+
+        checkArgumentsForNull(first, second);
+
         double resultRe = first.Re * second.Re - first.Im * second.Im;
         double resultIm = first.Re * second.Im + second.Re * first.Im;
 
         checkDoubleOverflow(resultRe, resultIm);
 
-        return new ComplexNumber(resultRe, resultIm);
+        return new ComplexNumbersImpl(resultRe, resultIm);
     }
 
-    public static double abs(ComplexNumber number) {
+    @Override
+    public double abs(ComplexNumbersImpl number) {
         double result = Math.sqrt(number.Re * number.Re + number.Im * number.Im);
         double scale = 1000;
         return Math.round(result * scale) / scale;
     }
 
-    private static void checkDoubleOverflow(double resultRe, double resultIm) {
+    private void checkArgumentsForNull(ComplexNumbersImpl first, ComplexNumbersImpl second) {
+        if (first == null || second == null) throw new NullPointerException("arguments is null");
+    }
+
+    private void checkDoubleOverflow(double resultRe, double resultIm) {
         if (resultRe == Double.POSITIVE_INFINITY || resultRe == Double.NEGATIVE_INFINITY ||
                 Double.isNaN(resultRe)) throw new ArithmeticException("double overflow");
 
@@ -61,7 +81,7 @@ public class ComplexNumber {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        ComplexNumber number = (ComplexNumber) o;
+        ComplexNumbersImpl number = (ComplexNumbersImpl) o;
         return Double.compare(number.Re, Re) == 0 && Double.compare(number.Im, Im) == 0;
     }
 
--- src/main/java/com/ivlevks/hw2/complex_numbers/ComplexNumbersTest.java
+++ src/main/java/com/ivlevks/hw2/complex_numbers/ComplexNumbersTest.java
@@ -2,12 +2,14 @@ package com.ivlevks.hw2.complex_numbers;
 
 public class ComplexNumbersTest {
     public static void main(String[] args) {
-        ComplexNumber num1 = new ComplexNumber(3, -6);
-        ComplexNumber num2 = new ComplexNumber(5, -8);
+        ComplexNumbersImpl num1 = null;
+        ComplexNumbersImpl num2 = new ComplexNumbersImpl(5, -8);
 
-        System.out.println(ComplexNumber.add(num1, num2));
-        System.out.println(ComplexNumber.subtract(num1, num2));
-        System.out.println(ComplexNumber.multiply(num1, num2));
-        System.out.println(ComplexNumber.abs(num1));
+        ComplexNumbers complexNumbers = new ComplexNumbersImpl();
+
+        System.out.println(complexNumbers.add(num1, num2));
+        System.out.println(complexNumbers.subtract(num1, num2));
+        System.out.println(complexNumbers.multiply(num1, num2));
+        System.out.println(complexNumbers.abs(num1));
     }
 }

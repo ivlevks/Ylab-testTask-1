c12411fcb113bb83814fb4f291330227e7f0c746
ivlevks <ivlevks@yandex.ru>
2023-03-18 13:32:19 +0300

    transliterator

--- src/main/java/com/ivlevks/hw3/transliterator/TransliteratorImpl.java
+++ src/main/java/com/ivlevks/hw3/transliterator/TransliteratorImpl.java
@@ -1,8 +1,31 @@
 package com.ivlevks.hw3.transliterator;
 
-public class TransliteratorImpl implements Transliterator{
+import java.util.*;
+
+public class TransliteratorImpl implements Transliterator {
+    private final Map<Character, String> translitedMap = new HashMap<>();
+
+    public TransliteratorImpl() {
+        List<Character> originalWord = Arrays.asList('А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н',
+                'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ы', 'Ь', 'Ъ', 'Э', 'Ю', 'Я');
+        for (int i = 0; i < originalWord.size(); i++) {
+            List<String> translitedWord = Arrays.asList("A", "B", "V", "G", "D", "E", "E", "ZH", "Z", "I", "I", "K", "L", "M", "N",
+                    "O", "P", "R", "S", "T", "U", "F", "KH", "TS", "CH", "SH", "SHCH", "Y", "", "IE", "E", "IU", "IA");
+            translitedMap.put(originalWord.get(i), translitedWord.get(i));
+        }
+    }
+
     @Override
     public String transliterate(String source) {
-        return null;
+        StringBuilder result = new StringBuilder();
+
+        char[] chars = source.toCharArray();
+        for (char character : chars) {
+            if (translitedMap.containsKey(character)) {
+                result.append(translitedMap.get(character));
+            } else result.append(character);
+        }
+
+        return result.toString();
     }
 }
--- src/main/java/com/ivlevks/hw3/transliterator/TransliteratorTest.java
+++ src/main/java/com/ivlevks/hw3/transliterator/TransliteratorTest.java
@@ -3,8 +3,39 @@ package com.ivlevks.hw3.transliterator;
 public class TransliteratorTest {
     public static void main(String[] args) {
         Transliterator transliterator = new TransliteratorImpl();
+
         String res = transliterator
                 .transliterate("HELLO! ПРИВЕТ! Go, boy!");
+        System.out.println(res);
+        res = transliterator
+                .transliterate("ДЕРЖИМОРДА");
+        System.out.println(res);
+        res = transliterator
+                .transliterate("СИМЕОНОВ-ПИЩИК");
+        System.out.println(res);
+        res = transliterator
+                .transliterate("ЧАЦКИЙ");
+        System.out.println(res);
+        res = transliterator
+                .transliterate("ШПЕКИН");
+        System.out.println(res);
+        res = transliterator
+                .transliterate("ВРАЛЬМАН");
+        System.out.println(res);
+        res = transliterator
+                .transliterate("ЯИЧНИЦА");
+        System.out.println(res);
+        res = transliterator
+                .transliterate("БУЛЬБА");
+        System.out.println(res);
+        res = transliterator
+                .transliterate("ЛЮЛЮКОВ");
+        System.out.println(res);
+        res = transliterator
+                .transliterate("ТЕСТтестТЕСТ");
+        System.out.println(res);
+        res = transliterator
+                .transliterate("ДОБЧИНСКИЙ");
         System.out.println(res);
     }
 }

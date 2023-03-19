ad67b8d0dbefd28dfbd98d5f68885e4b6cae0611
ivlevks <ivlevks@yandex.ru>
2023-03-19 17:56:34 +0300

    base sort solution

--- src/main/java/com/ivlevks/hw3/file_sort/Sorter.java
+++ src/main/java/com/ivlevks/hw3/file_sort/Sorter.java
@@ -1,10 +1,134 @@
 package com.ivlevks.hw3.file_sort;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.util.*;
 
 public class Sorter {
+    int counterForJoin = 1;
+
     public File sortFile(File dataFile) throws IOException {
-        return null;
+//        long sizeOfChunk = Runtime.getRuntime().maxMemory() / 2;
+        long sizeOfChunk = 10_000_000L;
+
+        List<File> listFile = divideFileOnChunk(dataFile, sizeOfChunk);
+        System.out.println("Complete divide");
+        sortEachChunck(listFile);
+        File result = mergeAllChunck(listFile);
+        return result;
+    }
+
+    private File mergeAllChunck(List<File> listFile) throws IOException {
+        Deque<File> deque = new ArrayDeque<>();
+        for (File file : listFile) {
+            deque.offer(file);
+        }
+
+        while (deque.size() != 1) {
+            File first = deque.poll();
+            File second = deque.poll();
+            File mergedFile = mergeTwoFiles(first, second);
+            System.out.println("merge" + first.getName() + " " + second.getName());
+            deque.offer(mergedFile);
+        }
+
+        return deque.getFirst();
+    }
+
+    private File mergeTwoFiles(File firstFile, File secondFile) throws IOException {
+        File result = new File(firstFile.getParentFile(), "joinData_" + counterForJoin++);
+
+        Scanner scanFirstFile = new Scanner(new FileInputStream(firstFile));
+        Scanner scanSecondFile = new Scanner(new FileInputStream(secondFile));
+        PrintWriter writer = new PrintWriter(result);
+
+        long value1 = 0;
+        long value2 = 0;
+        boolean writeFirstValue = true;
+        boolean writeSecondValue = true;
+        while (scanFirstFile.hasNextLong() && scanSecondFile.hasNextLong()) {
+            if (writeFirstValue) {
+                value1 = scanFirstFile.nextLong();
+            }
+            if (writeSecondValue) {
+                value2 = scanSecondFile.nextLong();
+            }
+
+            if (value1 <= value2) {
+                writer.println(value1);
+                writeFirstValue = true;
+                writeSecondValue = false;
+            } else {
+                writer.println(value2);
+                writeFirstValue = false;
+                writeSecondValue = true;
+            }
+        }
+
+        while (scanFirstFile.hasNextLong()) {
+            value1 = scanFirstFile.nextLong();
+            writer.println(value1);
+        }
+        while (scanSecondFile.hasNextLong()) {
+            value2 = scanSecondFile.nextLong();
+            writer.println(value2);
+        }
+
+        scanFirstFile.close();
+        scanSecondFile.close();
+        writer.close();
+
+        return result;
+    }
+
+
+    private void sortEachChunck(List<File> listFile) throws IOException {
+        System.out.println("Start sort divided file");
+        List<Long> list = new ArrayList<>();
+
+        for (File file : listFile) {
+            list.clear();
+            try (FileInputStream fileInputStream = new FileInputStream(file);
+                 Scanner scanner = new Scanner(fileInputStream)) {
+                while (scanner.hasNextLong()) {
+                    long number = scanner.nextLong();
+                    list.add(number);
+                }
+            }
+
+            Collections.sort(list);
+
+            try (PrintWriter printWriter = new PrintWriter(file)) {
+                for (Long num : list) {
+                    printWriter.println(num);
+                    printWriter.flush();
+                }
+            }
+            System.out.println("sorted " + file.getName());
+        }
+    }
+
+    private List<File> divideFileOnChunk(File dataFile, long sizeOfChunk) throws IOException {
+        List<File> result = new ArrayList<>();
+        int counter = 1;
+
+        try (FileInputStream fileInputStream = new FileInputStream(dataFile);
+             Scanner scanner = new Scanner(fileInputStream)) {
+
+            while (scanner.hasNextLong()) {
+                File newFile = new File(dataFile.getParentFile(), "tempData_" + counter++);
+                try (PrintWriter printWriter = new PrintWriter(newFile)) {
+
+                    while (scanner.hasNextLong()) {
+                        long number = scanner.nextLong();
+                        if (newFile.length() + 8 > sizeOfChunk) break;
+                        printWriter.println(number);
+                        printWriter.flush();
+                    }
+                    result.add(newFile);
+                    System.out.println("created " + newFile.getName());
+                }
+            }
+        }
+        return result;
     }
 }
--- src/main/java/com/ivlevks/hw3/file_sort/Test.java
+++ src/main/java/com/ivlevks/hw3/file_sort/Test.java
@@ -5,9 +5,24 @@ import java.io.IOException;
 
 public class Test {
     public static void main(String[] args) throws IOException {
-        File dataFile = new Generator().generate("data.txt", 375_000_000);
-        System.out.println(new Validator(dataFile).isSorted()); // false
+//        File dataFile = new Generator().generate("data.txt", 3_000_000);
+        File dataFile = new File("data.txt");
+//        System.out.println(new Validator(dataFile).isSorted()); // false
         File sortedFile = new Sorter().sortFile(dataFile);
-        System.out.println(new Validator(sortedFile).isSorted()); // true
+//        System.out.println(new Validator(sortedFile).isSorted()); // true
+
+
+//        // Get current size of heap in bytes
+//        long heapSize = Runtime.getRuntime().totalMemory() / 1000 / 1000;
+//
+//        // Get maximum size of heap in bytes. The heap cannot grow beyond this size.// Any attempt will result in an OutOfMemoryException.
+//        long heapMaxSize = Runtime.getRuntime().maxMemory() / 1000 / 1000;
+//
+//        // Get amount of free memory within the heap in bytes. This size will increase // after garbage collection and decrease as new objects are created.
+//        long heapFreeSize = Runtime.getRuntime().freeMemory() / 1000 / 1000;
+//
+//        System.out.println(heapSize);
+//        System.out.println(heapMaxSize);
+//        System.out.println(heapFreeSize);
     }
 }

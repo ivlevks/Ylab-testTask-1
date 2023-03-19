48171a3be0b176e0d1dfc5491175d968baedc1cc
ivlevks <ivlevks@yandex.ru>
2023-03-19 12:26:22 +0300

    org structure

--- src/main/java/com/ivlevks/hw3/org_structure/OrgStructureParserImpl.java
+++ src/main/java/com/ivlevks/hw3/org_structure/OrgStructureParserImpl.java
@@ -1,12 +1,58 @@
 package com.ivlevks.hw3.org_structure;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class OrgStructureParserImpl implements OrgStructureParser {
 
     @Override
     public Employee parseStructure(File csvFile) throws IOException {
-        return null;
+        /*
+          Storage Employee in format: Key - id, Value - Employee
+         */
+        Map<Long, Employee> employeeMap = new HashMap<>();
+
+        try (FileInputStream fileInputStream = new FileInputStream(csvFile);
+             Scanner scanner = new Scanner(fileInputStream)) {
+            while (scanner.hasNextLine()) {
+                String stringFromFile = scanner.nextLine();
+                String[] split = stringFromFile.split(";");
+
+                if (split[0].equals("id")) continue;
+                Long id = Long.valueOf(split[0]);
+                Long bossId = split[1].equals("") ? null : Long.valueOf(split[1]);
+                String name = split[2];
+                String position = split[3];
+
+                //get or create employee
+                Employee employee;
+                if (employeeMap.containsKey(id)) {
+                    employee = employeeMap.get(id);
+                } else employee = new Employee();
+
+                //set parameters employee
+                employee.setId(id);
+                employee.setBossId(bossId);
+                employee.setName(name);
+                employee.setPosition(position);
+                employeeMap.put(id, employee);
+
+                //get or create Boss of employee,
+                // add in list subordinate current employee
+                if (employeeMap.containsKey(bossId)) {
+                    employee.setBoss(employeeMap.get(bossId));
+                    employeeMap.get(bossId).getSubordinate().add(employee);
+                } else if (bossId != null){
+                    Employee employeeBoss = new Employee();
+                    employeeBoss.getSubordinate().add(employee);
+                    employeeMap.put(bossId, employeeBoss);
+                }
+            }
+        }
+        return employeeMap.get(1L);
     }
 }
--- src/main/java/com/ivlevks/hw3/org_structure/OrgStructureTest.java
+++ src/main/java/com/ivlevks/hw3/org_structure/OrgStructureTest.java
@@ -1,4 +1,34 @@
 package com.ivlevks.hw3.org_structure;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
 public class OrgStructureTest {
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        OrgStructureParserImpl parser = new OrgStructureParserImpl();
+        Employee boss = parser.parseStructure(getFileFromResources());
+
+        String offset = "";
+        printStructure(boss, offset);
+    }
+
+    private static void printStructure(Employee boss, String offset) {
+        System.out.printf("%sId - %d; bossId - %d; name - %s; position - %s%n",
+                offset, boss.getId(), boss.getBossId(), boss.getName(), boss.getPosition());
+        if (!boss.getSubordinate().isEmpty()) {
+            offset += "      ";
+            for (Employee employee : boss.getSubordinate()) {
+                printStructure(employee, offset);
+            }
+        }
+    }
+
+    public static File getFileFromResources() throws URISyntaxException {
+//        URL url = OrgStructureTest.class.getClassLoader().getResource("file.csv");
+//        URL url = OrgStructureTest.class.getClassLoader().getResource("file2.csv");
+        URL url = OrgStructureTest.class.getClassLoader().getResource("file3.csv");
+        return new File(url.toURI().getPath());
+    }
 }

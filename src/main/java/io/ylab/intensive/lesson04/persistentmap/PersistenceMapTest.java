3401b5793e12d7bc56db9ba5396da4fc8205cfe1
ivlevks <ivlevks@yandex.ru>
2023-03-27 23:22:33 +0300

    start with  persistent Map

--- src/main/java/io/ylab/intensive/lesson04/persistentmap/PersistenceMapTest.java
+++ src/main/java/io/ylab/intensive/lesson04/persistentmap/PersistenceMapTest.java
@@ -6,22 +6,27 @@ import javax.sql.DataSource;
 import java.sql.SQLException;
 
 public class PersistenceMapTest {
-  public static void main(String[] args) throws SQLException {
-    DataSource dataSource = initDb();
-    PersistentMap persistentMap = new PersistentMapImpl(dataSource);
-    // Написать код демонстрации работы 
-  }
-  
-  public static DataSource initDb() throws SQLException {
-    String createMapTable = "" 
-                                + "drop table if exists persistent_map; " 
-                                + "CREATE TABLE if not exists persistent_map (\n"
-                                + "   map_name varchar,\n"
-                                + "   KEY varchar,\n"
-                                + "   value varchar\n"
-                                + ");";
-    DataSource dataSource = DbUtil.buildDataSource();
-    DbUtil.applyDdl(createMapTable, dataSource);
-    return dataSource;
-  }
+    public static void main(String[] args) throws SQLException {
+        DataSource dataSource = initDb();
+        PersistentMap persistentMap = new PersistentMapImpl(dataSource);
+        persistentMap.init("map");
+        persistentMap.put("key1", "value1");
+        System.out.println(persistentMap.containsKey("key1"));
+        System.out.println(persistentMap.containsKey("key2"));
+
+        // Написать код демонстрации работы
+    }
+
+    public static DataSource initDb() throws SQLException {
+        String createMapTable = ""
+                + "drop table if exists persistent_map; "
+                + "CREATE TABLE if not exists persistent_map (\n"
+                + "   map_name varchar,\n"
+                + "   KEY varchar,\n"
+                + "   value varchar\n"
+                + ");";
+        DataSource dataSource = DbUtil.buildDataSource();
+        DbUtil.applyDdl(createMapTable, dataSource);
+        return dataSource;
+    }
 }
--- src/main/java/io/ylab/intensive/lesson04/persistentmap/PersistentMapImpl.java
+++ src/main/java/io/ylab/intensive/lesson04/persistentmap/PersistentMapImpl.java
@@ -1,52 +1,84 @@
 package io.ylab.intensive.lesson04.persistentmap;
 
 import javax.sql.DataSource;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Scanner;
 
 /**
- * Класс, методы которого надо реализовать 
+ * Класс, методы которого надо реализовать
  */
 public class PersistentMapImpl implements PersistentMap {
-  
-  private DataSource dataSource;
 
-  public PersistentMapImpl(DataSource dataSource) {
-    this.dataSource = dataSource;
-  }
+    private DataSource dataSource;
+    private String mapName;
 
-  @Override
-  public void init(String name) {
-    
-  }
+    public PersistentMapImpl(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
-  @Override
-  public boolean containsKey(String key) throws SQLException {
-    return false;
-  }
+    @Override
+    public void init(String name) {
+        this.mapName = name;
+    }
 
-  @Override
-  public List<String> getKeys() throws SQLException {
-    return null;
-  }
+    @Override
+    public boolean containsKey(String key) throws SQLException {
+        boolean result = false;
+        String containsKey = "SELECT EXISTS(SELECT * from persistent_map WHERE map_name=? AND KEY=?)";
 
-  @Override
-  public String get(String key) throws SQLException {
-    return null;
-  }
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(containsKey)) {
 
-  @Override
-  public void remove(String key) throws SQLException {
+            preparedStatement.setString(1, mapName);
+            preparedStatement.setString(2, key);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+            result = resultSet.getObject(1, Boolean.class);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
-  }
+        return result;
+    }
 
-  @Override
-  public void put(String key, String value) throws SQLException {
+    @Override
+    public List<String> getKeys() throws SQLException {
+        return null;
+    }
 
-  }
+    @Override
+    public String get(String key) throws SQLException {
+        return null;
+    }
 
-  @Override
-  public void clear() throws SQLException {
+    @Override
+    public void remove(String key) throws SQLException {
 
-  }
+    }
+
+    @Override
+    public void put(String key, String value) throws SQLException {
+        String put = "INSERT INTO persistent_map (map_name, KEY, value) VALUES (?, ?, ?)";
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(put)) {
+            preparedStatement.setString(1, mapName);
+            preparedStatement.setString(2, key);
+            preparedStatement.setString(3, value);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void clear() throws SQLException {
+
+    }
 }

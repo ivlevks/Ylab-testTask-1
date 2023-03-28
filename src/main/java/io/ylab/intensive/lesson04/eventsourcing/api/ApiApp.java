b9a77f5dcbbd48b7307937f5627df63851860dad
ivlevks <ivlevks@yandex.ru>
2023-03-28 21:18:45 +0300

    base functional RabbitMQ

--- src/main/java/io/ylab/intensive/lesson04/eventsourcing/api/ApiApp.java
+++ src/main/java/io/ylab/intensive/lesson04/eventsourcing/api/ApiApp.java
@@ -1,15 +1,30 @@
 package io.ylab.intensive.lesson04.eventsourcing.api;
 
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import io.ylab.intensive.lesson04.RabbitMQUtil;
 
 public class ApiApp {
-  public static void main(String[] args) throws Exception {
-    ConnectionFactory connectionFactory = initMQ();
-    // Тут пишем создание PersonApi, запуск и демонстрацию работы
-  }
+    public static void main(String[] args) throws Exception {
+        String exchangeName = "exc";
+        String queueName = "queue";
 
-  private static ConnectionFactory initMQ() throws Exception {
-    return RabbitMQUtil.buildConnectionFactory();
-  }
+        ConnectionFactory connectionFactory = initMQ();
+        // Тут пишем создание PersonApi, запуск и демонстрацию работы
+
+        try (Connection connection = connectionFactory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.exchangeDeclare(exchangeName, BuiltinExchangeType.TOPIC);
+            channel.queueDeclare(queueName, true, false, false, null);
+            channel.queueBind(queueName, exchangeName, "*");
+
+            channel.basicPublish(exchangeName, "*", null, "Hello world1111".getBytes());
+        }
+    }
+
+    private static ConnectionFactory initMQ() throws Exception {
+        return RabbitMQUtil.buildConnectionFactory();
+    }
 }
